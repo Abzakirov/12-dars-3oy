46b8991ae9c6fa1d075f0621b657700283cee59c
AbdullohZokirov <abdullazokirov808@gmail.com>
2024-12-13 10:54:17 +0500

    12dars

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// function arrayMadness(a, b) {
+//   if (a.length >= 1 && b.length >= 1) {
+//     let c = a.reduce((sum, num) => sum + num ** 2, 0);
+//     let d = b.reduce((sum, num) => sum + num ** 3, 0);
+
+//     return c > d;
+//   }
+
+//   return false;
+// }
+
+// console.log(arrayMadness([4, 5, 6], [1, 2, 3]));
+// console.log(arrayMadness([1, 2], [3, 4]));
+// console.log(arrayMadness([], []));
+
+// function printArray(array) {
+//     return array.join(",");
+//   }
+
+//   console.log(printArray(["h", "o", "l", "a"]));
+
+// var findAverage = function (num) {
+//     let sum = num.reduce((acc, val) => acc + val, 0);
+  
+//     let result = sum / num.length;
+  
+//     return result;
+//   };
+//   let numbers = [1, 3, 5, 7];
+  
+//   console.log(findAverage(numbers));
+  
+
+// function stringToArray(string){
+
+// let str = string.split(" ")
+	
+
+// }
+//  let string = "I love arrays they are my favorite"
+
+
+// console.log(stringToArray(string));
+
+    
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        kora: "#1A1A1A",
+        underline:"#A0A747"
+      },
+    },
+  },
+  plugins: [],
+};
